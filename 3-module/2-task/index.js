376e65bd25b6f157d9ce49e8ccf8442ea558f096
lartem647 <lartem647@gmail.com>
2019-09-20 16:10:45 +0300

    решена задача

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,20 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
 function getMinMax(str) {
+  const obj = {};
+  const result = [];
+  const newArray = str.split(' ');
+  const newString = newArray.join(',');
+  const num = newString.split(',');
+  for (const key of num) {
+    if (!Number.isNaN(parseFloat(key))) {
+      result.push(parseFloat(key));
+    }
+  }
+  obj.min = Math.min(...result);
+  obj.max = Math.max(...result);
+  return (obj);
 }
+
